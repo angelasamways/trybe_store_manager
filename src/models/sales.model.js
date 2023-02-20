dc646bcfb6a1d32fce23842cd5b051835a7b4b59
Angela Samways <angelasamways@gmail.com>
2023-02-20 17:24:49 -0300

    Req8-fix:lint

--- src/models/sales.model.js
+++ src/models/sales.model.js
@@ -1,15 +1,18 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-  const [result] = await connection.execute(
-    'SELECT sal.id AS saleId, sal.date, pro.product_id AS productId, pro.quantity FROM StoreManager.sales_products AS pro LEFT JOIN StoreManager.sales AS sal ON sal.id = pro.sale_id',
-  );
+  const query = `SELECT sal.id AS saleId, sal.date, pro.product_id AS productId, pro.quantity 
+  FROM StoreManager.sales_products AS pro LEFT JOIN StoreManager.sales AS sal 
+  ON sal.id = pro.sale_id`;
+  const [result] = await connection.execute(query);
 
   return result;
 };
 
 const getById = async (id) => {
-  const query = `SELECT sal.date, sp.product_id AS productId, sp.quantity FROM StoreManager.sales_products AS sp LEFT JOIN StoreManager.sales AS sal ON sal.id = sp.sale_id WHERE sal.id = ${id} ORDER BY sal.id, sp.product_id ASC`;
+  const query = `SELECT sal.date, sp.product_id AS productId, sp.quantity FROM 
+  StoreManager.sales_products AS sp LEFT JOIN StoreManager.sales AS sal ON sal.id
+  = sp.sale_id WHERE sal.id = ${id} ORDER BY sal.id, sp.product_id ASC`;
   const [result] = await connection.execute(query);
   return result;
 };
